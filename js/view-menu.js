0eed890f73c1f52e47693d5f48943df3dcb79ffd
JDavid80 <jdcalderong@gmail.com>
2021-09-01 17:46:26 -0500

    add grid

--- js/view-menu.js
+++ js/view-menu.js
@@ -1,6 +1,4 @@
 function mostrarMenu() {
-/*     var navMostrar = document.getElementById("side-header");
-    navMostrar.classList.toggle("mostrarHeader"); */
     var navMostrar = document.getElementById("menu-principal");
     navMostrar.classList.toggle("mostrarNav");
 }
