13bb1ea4cbb54b078d1c18d24874e1c187f51627
AMBIENTEY13 <AMBIENTEY13@PITAPRTNNFSD193.FormacionSena.RED>
2022-02-10 14:10:02 -0500

    add configurando texto

---
+++ javascript/js/27-configurandoTexto.js
@@ -0,0 +1,7 @@
+function expandirTexto() {
+    let textoCompleto = 'Esta semana, hace cuatro años, el cohete operativo más poderoso del mundo, el Falcon Heavy de SpaceX, hizo su lanzamiento inaugural desde Florida y se lanzó al espacio exterior transportando nada más que el Tesla roadster personal de Elon Musk.<br>' + 
+    'El auto deportivo rojo cereza, que está ocupado por un maniquí vestido con un traje espacial apodado "Starman", todavía está afuera, tomando un camino orbital oblongo y solitario alrededor del Sol, viajando tan lejos como la órbita de Marte y, en otros momentos, tan cerca como la órbita de la Tierra';
+
+
+    document.getElementById("texto-completo").innerHTML = textoCompleto;
+}
